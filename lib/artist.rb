a74810fe243db719dc97bc751b3f623d9e10aea6
Janay Bailey <tjanay87@gmail.com>
2019-10-14 20:05:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,8 @@
 require 'pry'
-
 class Artist
-  attr_accessor :name, :song
-
-  @@song_count = 0
-
-  def initialize(name, song)
+  attr_accessor :name
+ 
+  def initialize(name)
     @name = name
-    @songs = []
   end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def self.song_count
-    @@song_count
-  end
-
 end
